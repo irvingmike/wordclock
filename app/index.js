47bbab2e8cda0ae66705b6c7941117dbc5bfb3bd
Anderson, Aaron R <AANDERSO@amfam.com>
2017-04-14 12:38:37 -0500

    First commit with basic setup and initial files

---
+++ app/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Created by ara024 on 4/14/17.
+ */
+var angular = require('angular');
+
+angular.module('clockApp', [])
+  .controller('mainController', function($scope) {
+    let text="word clock";
+  });
